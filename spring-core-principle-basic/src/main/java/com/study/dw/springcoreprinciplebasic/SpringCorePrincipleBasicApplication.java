677ac6a72b8892adab03184cedc8016ee8781cea
dw <devdw98@gmail.com>
2021-06-03 17:47:06 +0900

    스프링 핵심원리 초기 세팅

---
+++ spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/SpringCorePrincipleBasicApplication.java
@@ -0,0 +1,13 @@
+package com.study.dw.springcoreprinciplebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorePrincipleBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCorePrincipleBasicApplication.class, args);
+	}
+
+}
