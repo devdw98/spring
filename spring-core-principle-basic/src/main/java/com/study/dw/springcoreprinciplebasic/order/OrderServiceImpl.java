24ae9c6b0c5446ee067b4b0b1974a675c7214652
dw <devdw98@gmail.com>
2021-06-10 17:20:10 +0900

    문제점 발견

--- spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/order/OrderServiceImpl.java
+++ spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.study.dw.springcoreprinciplebasic.order;
 
 import com.study.dw.springcoreprinciplebasic.discount.DiscountPolicy;
 import com.study.dw.springcoreprinciplebasic.discount.FixDixcountPolicy;
+import com.study.dw.springcoreprinciplebasic.discount.RateDiscountPolicy;
 import com.study.dw.springcoreprinciplebasic.member.Member;
 import com.study.dw.springcoreprinciplebasic.member.MemberRepository;
 import com.study.dw.springcoreprinciplebasic.member.MemoryMemberRepository;
@@ -9,8 +10,8 @@ import com.study.dw.springcoreprinciplebasic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDixcountPolicy();
-
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //할인정책 적용 시 이 부분을 변경해야함
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); //할인정책 적용 시 이 부분을 변경해야함
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
