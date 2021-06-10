68f0131852fe1640f8c53630db57f1c3444f01fb
dw <devdw98@gmail.com>
2021-06-10 18:04:32 +0900

    역할, 구현 분리

--- spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/AppConfig.java
+++ spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/AppConfig.java
@@ -1,6 +1,8 @@
 package com.study.dw.springcoreprinciplebasic;
 
+import com.study.dw.springcoreprinciplebasic.discount.DiscountPolicy;
 import com.study.dw.springcoreprinciplebasic.discount.FixDixcountPolicy;
+import com.study.dw.springcoreprinciplebasic.member.MemberRepository;
 import com.study.dw.springcoreprinciplebasic.member.MemberService;
 import com.study.dw.springcoreprinciplebasic.member.MemberServiceImpl;
 import com.study.dw.springcoreprinciplebasic.member.MemoryMemberRepository;
@@ -9,11 +11,21 @@ import com.study.dw.springcoreprinciplebasic.order.OrderServiceImpl;
 
 //Application 의 환경 구성에 대한 모든 것을 여기서 함
 public class AppConfig {
+    // 역할
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());// 생성자 주입
+        return new MemberServiceImpl(memberRepository());// 생성자 주입
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDixcountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+
+    //구현
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDixcountPolicy();
     }
 }
