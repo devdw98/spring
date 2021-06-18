41c5b2eb0016e764c4d362d0b08a989ed76f3dd6
dw <devdw98@gmail.com>
2021-06-18 18:25:34 +0900

    스프링 적용

--- spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/AppConfig.java
+++ spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/AppConfig.java
@@ -9,23 +9,28 @@ import com.study.dw.springcoreprinciplebasic.member.MemberServiceImpl;
 import com.study.dw.springcoreprinciplebasic.member.MemoryMemberRepository;
 import com.study.dw.springcoreprinciplebasic.order.OrderService;
 import com.study.dw.springcoreprinciplebasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//Application 의 환경 구성에 대한 모든 것을 여기서 함
-public class AppConfig {
+@Configuration
+public class AppConfig { //Application 의 환경 구성에 대한 모든 것을 여기서 함 (설정정보)
+
     // 역할
+    @Bean // spring container에 등록이 됨
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());// 생성자 주입
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
     //구현
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDixcountPolicy();
         return new RateDiscountPolicy(); //할인정책 변경
--- spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/MemberApp.java
+++ spring-core-principle-basic/src/main/java/com/study/dw/springcoreprinciplebasic/MemberApp.java
@@ -4,11 +4,19 @@ import com.study.dw.springcoreprinciplebasic.member.Grade;
 import com.study.dw.springcoreprinciplebasic.member.Member;
 import com.study.dw.springcoreprinciplebasic.member.MemberService;
 import com.study.dw.springcoreprinciplebasic.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext; //spring container(객체 모든 걸 관리)
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        // Spring으로 바꾸기
+        // AppConfig class에 있는 설정 정보를 가지고 spring이 @Bean에다가 관리해줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
